e043087b97e9e9d5949cb335d40a78a37e7c0b29
major-menphis <ssmenphis@hotmail.com>
2022-07-12 19:59:32 -0300

    add imports dinamicos

---
+++ modulo_2/js_modulos/README.md
@@ -0,0 +1,3 @@
+# JavaScript módulos
+
+Dentro deste repositório é possivel encontrar as atividades referentes a módulos JS propostas no curso.
---
+++ modulo_2/js_modulos/atividade/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Major", 34));
+console.log(mostraCidade("Major", "Uruguaiana"));
+console.log(mostraHobby("Major", "assistir filmes"));
